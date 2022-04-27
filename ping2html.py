519b59185eef41129013e5cc98786c0fa3b7e052
Eagle57 <94141086+Eagle57f@users.noreply.github.com>
2022-04-27 19:23:01 +0200

    Update ping2html.py

--- ping2html.py
+++ ping2html.py
@@ -2,6 +2,7 @@ import os, tkinter
 from time import strftime, sleep
 from datetime import datetime
 from threading import Thread
+from HTML_CSS import HTML_CSS
 
 exit=False
 numberFichier = 1
@@ -24,27 +25,9 @@ def fexit():
 def prg():
     global numberFichier, totalcoupures
     var=True
-    while True:
-        if os.path.isfile(f"{os.path.dirname(__file__)}\\docping-{numberFichier}.html") == False:
-            fichier = open(f"{os.path.dirname(__file__)}\\docping{numberFichier}.html", "w", encoding="utf-8")
-            break
-        else:
-            numberFichier += 1
-    
+    fichier = open(f"{os.path.dirname(__file__)}\\docping - en cours.html", "r+", encoding="utf-8")
     fichierpath = os.path.dirname(__file__)
     fichier.truncate(0)
-    fichier.write('''
-<html>
-    <head>
-        <meta charset="utf-8">
-        <link rel="stylesheet"href="docping.css">
-        <title>Ping</title>
-    </head>
-    <body>
-        <header>
-            <h1>Résultats du test de coupures de connection</h1>
-        </header>
-''')
     heuredebuttest = datetime.now()
     dureecoupures = 0
     nombrecoupures = 0
@@ -56,7 +39,7 @@ def prg():
 
         if response == 1 and var == True:
             heuredebut=a
-            totalcoupures = totalcoupures + f"<p><strong>Début de coupure:</strong> {heure}</p>"
+            totalcoupures = totalcoupures + f'<p class="coupure"><strong>Début de coupure:</strong> {heure}</p>'
             var=False
 
         if response == 0 and var == False:
@@ -66,10 +49,8 @@ def prg():
                 dureecoupures = heurefin - heuredebut
             else:
                 dureecoupures = dureecoupures + (heurefin - heuredebut)
-            totalcoupures = totalcoupures + f"<p><strong>Fin de coupure:</strong> {heure}\tDurée de coupure: {str(heurefin - heuredebut)}</p>"
+            totalcoupures = totalcoupures + f'<p class="coupure"><strong>Fin de coupure:</strong> {heure}\tDurée de coupure: {str(heurefin - heuredebut)}</p>'
             nombrecoupures += 1
-            fichier.close()
-            fichier = open(f"{os.path.dirname(__file__)}\\docping{numberFichier}.html", "a", encoding="utf-8")
             var=True
     dureetest = datetime.now() - heuredebuttest
     if nombrecoupures != 0:
@@ -77,83 +58,17 @@ def prg():
     else:
         moyennecoupures = "0"
     heurefintest = datetime.now()
-    fichier.write(f"""
-        <div class="bilan">
-            <p>---------------------------------------------------------------<br>
-            <strong>Début du test:</strong> {heuredebuttest}<br>
-            <strong>Fin du test:</strong> {heurefintest}<br>
-            <strong>Durée du test:</strong> {dureetest}<br><br>
-            <strong>Nombre de coupures:</strong> {nombrecoupures}<br>
-            <strong>Durée totale des coupures:</strong> {dureecoupures}<br>
-            <strong>Durée moyenne des coupures:</strong> {moyennecoupures}<br>
-            ---------------------------------------------------------------
-            </p>
-        </div>
-        <div class="container">
-            {totalcoupures}
-        </div>
-    </body>
-</html>
-""")
-    print(totalcoupures)
-    fichier.close()
     heurefintestchange = heurefintest.strftime("%Y-%m-%d %H-%M-%S")
-    os.rename(f"{fichierpath}\\docping{numberFichier}.html", f"{fichierpath}\\docping - {heurefintestchange}.html")
+    fichier.write(HTML_CSS.fHTML(heuredebuttest, heurefintest, dureetest, nombrecoupures, dureecoupures, moyennecoupures, totalcoupures, heurefintestchange))
+    fichier.close()
+    os.rename(f"{fichierpath}\\docping - en cours.html", f"{fichierpath}\\docping - {heurefintestchange}.html")
     
 ftkinter()
 
 def css():
     fcss = open(f"{os.path.dirname(__file__)}\\docping.css", "w", encoding="utf-8")
     fcss.truncate(0)
-    fcss.write('''
-body {
-    background-color: #2b2b2b;
-    color: #ffffff;
-    font-family: "Courier New", Courier, monospace;
-    font-size: 12px;
-    text-align: center;
-}
-p {
-    font-size: 12px;
-}
-
-header
-{
-    position: absolute;
-    font-family:'Arial Narrow Bold', sans-serif;
-    width: 100%;
-    left: 0px;
-    top: 0px;
-    height: 60px;
-    background-color: #4389da;
-}
-
-.bilan
-{
-    position: absolute;
-    width: 100%;
-    right: 0px;
-    height: 150px;
-    top: 60px;
-    background-color: #5a86ff;
-}
-.bilan:hover {
-    color: #83fbff;
-
-}
-.container
-{
-    position: absolute;
-    width: 100%;
-    right: 0px;
-    top: 210px;
-    background-color: #366cff;
-}
-
-.container:hover {
-    color: #c27efa;
-
-}''')
+    fcss.write(HTML_CSS.fCSS())
     fcss.close()
 
 css()
